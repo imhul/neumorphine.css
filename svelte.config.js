59a8c2b6e22aad013822375513414bafefbb4b44
tkachuk <z.tkachuk@nodeart.io>
2024-01-31 01:02:51 +0200

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapterGhpages from 'svelte-adapter-ghpages';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
@@ -16,11 +16,7 @@ const config = {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapterGhpages({
-            pages: 'build',
-            assets: 'build',
-            fallback: null
-        })
+        adapter: adapter()
     }
 };
 
