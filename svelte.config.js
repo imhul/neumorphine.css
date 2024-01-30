bd61912d94dad1f3db97a6b834f68b2097cfb946
tkachuk <z.tkachuk@nodeart.io>
2024-01-30 23:43:20 +0200

    preparing to deploy

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
     kit: {
         paths: {
             base: '/neumorphine.css',
-            relative: false
+            assets: '/neumorphine.css'
         },
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
